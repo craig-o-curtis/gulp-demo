ab8b4a4c5924dd31a183f41b86c329ec1878d8ea
Craig Curtis <ccurtis@nextcenturi.com>
2017-05-03 11:05:33 -0700

    Connect gulp-uglify to minify js

--- gulpfile.js
+++ gulpfile.js
@@ -153,15 +153,22 @@ gulp.task('optimize', ['inject'], function() {
     var assets = $.useref.assets({searchPath: './'}); // gather assets from <!-- -->
     var templateCache = config.temp + config.templateCache.file;
     var cssFilter = $.filter('**/*.css', {restore: true});
+    var jsFilter = $.filter('**/*.js', {restore: true});
 
     return gulp
         .src(config.index)
         .pipe($.plumber()) // error handling
         .pipe($.inject(gulp.src(templateCache, {read: false}), {starttag: '<!-- inject:templates:js -->'} )) // inject template.js into index.html
         .pipe(assets) // get js and css files from <!-- build:* --><!-- endbuild -->
+
         .pipe(cssFilter) // filter down to css
         .pipe($.csso()) // csso
         .pipe(cssFilter.restore) // filter restore to get all files back
+
+        .pipe(jsFilter) // filter down to js
+        .pipe($.uglify())
+        .pipe(jsFilter.restore) // restore files back
+
         .pipe(assets.restore()) // get index.html back
         .pipe($.useref()) // replaces with one-liners for app.* and lib.* assets
         .pipe(gulp.dest(config.dist));
